88447bc84d8e1ccefca505977256bc910ac71776
Karmel0x <Archerxplx@gmail.com>
2024-02-23 12:28:28 +0100

    ts

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const navmeshcppjs = require('./bindings/build/Release/navmeshcppjs.node');
 
 
 class Navmeshcppjs {
-	static initialize(inflate = 0){
+	static initialize(inflate = 0) {
 		navmeshcppjs.initialize(inflate);
 	}
-	static getPath(startPoint, endPoint){
+
+	/**
+	 * @param {import('./index2').Point} startPoint 
+	 * @param {import('./index2').Point} endPoint 
+	 * @returns {import('./index2').Point[]}
+	 */
+	static getPath(startPoint, endPoint) {
 		return navmeshcppjs.pf(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
 	}
 }
